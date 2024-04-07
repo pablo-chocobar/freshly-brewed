80ac4892603d73911e1f60486c15bc1f4970e51c
pablo-chocobar <iampranavpr@gmail.com>
2024-04-07 18:14:49 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,20 +1,26 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 function Navbar() {
   return (
-    <div className="overflow-hidden bg-transparent flex flex-row items-center justify-start py-6 px-8 box-border gap-2">
-      <div className="tracking-tight leading-3 font-semibold text-content1">
-        THE
-      </div>
-      <div className="bg-content1 flex flex-row items-center justify-center py-1 px-1 text-background">
-        <div className="tracking-tight leading-3 font-semibold">
-          PRODUCT
+    <div className="overflow-hidden bg-transparent  py-6 px-8 box-border max-w-max">
+      <Link to = "/">
+        <div className="flex flex-row items-center justify-start gap-2 max-w-max">
+          <div className="tracking-tight leading-3 font-semibold text-content1">
+            THE
+          </div>
+          <div className="bg-content1 flex flex-row items-center justify-center py-1 px-1 text-background">
+            <div className="tracking-tight leading-3 font-semibold">
+              PRODUCT
+            </div>
+          </div>
+          <div className="tracking-tight leading-3 font-semibold text-content1">
+            PLATFORM
+          </div>
         </div>
-      </div>
-      <div className="tracking-tight leading-3 font-semibold text-content1">
-        PLATFORM
-      </div>
-    </div>)
+      </Link>
+    </div>
+  )
 }
 
 export default Navbar
